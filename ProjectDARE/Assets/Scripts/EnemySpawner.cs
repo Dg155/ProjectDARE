e01711b1dba343c19767547cbeef2ef65c0e73c6
Trevor Vahan <tvahan@uci.edu>
2022-10-22 17:39:25 -0700

    Added the enemy spawner script
    Enemy Spawner has a few fields, one for the house object, one for the enemy to be spawned, and then other adjustable values

---
+++ ProjectDARE/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject house;
+    [SerializeField] private GameObject enemy;
+    [SerializeField] private float spawnRadius = 7f;
+    [SerializeField] private float totalEnemies = 10f;
+    [SerializeField] private float spawnInterval = 2.5f;
+
+    
+    void Start()
+    {
+        SpawnEnemy();
+    }
+
+
+    async void SpawnEnemy()
+    {
+        for (int i = 0; i < totalEnemies; ++i)
+        {
+            Vector2 spawnPosition = house.transform.position; 
+            spawnPosition += Random.insideUnitCircle.normalized * spawnRadius;
+            Instantiate(enemy, spawnPosition, Quaternion.identity);
+            Debug.LogFormat("Spawned enemy at time {0}", Time.time);
+            await Task.Delay((int)(spawnInterval * 1000));
+        }
+        Debug.LogFormat("Finished spawning enemies at {0}", Time.time);
+    }
+
+}
